3ed339535a93c1201d2322e54f25c097015fef69
Akuby <akuby1997@gmail.com>
2022-03-25 22:43:20 +0900

    applying styles

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>{{movie.imdbID}}</div>
+  <div 
+  :style="{ backgroundImage : `url(${movie.Poster})` }"
+  class="movie">
+  <div class="info">
+    <div class="year">
+    {{movie.Year}}
+  </div>
+  <div class="title">
+    {{movie.Title}}
+  </div>
+  </div>
+  </div>
 </template>
 
 <script>
@@ -12,3 +23,48 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "~/scss/main";
+.movie{
+  $width : 200px;
+  width: $width;
+  height: $width * 3/2;
+  position: relative;
+  margin: 10px;
+  border-radius: 4px;
+  background-color: $gray-400;
+  background-size: cover;
+  overflow: hidden;
+  transition: 1s;
+  &:hover::after{
+    content: "";
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    border: 6px solid $primary;
+  }
+  .info{
+    padding: 10px;
+    background-color: rgba($black, .3);
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    font-size: 14px;
+    text-align: center;
+    width: 100%;
+    backdrop-filter: blur(10px);
+    .year{
+      color: $primary;
+    }
+    .title{
+      color : $white;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+  }
+}
+</style>
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="container">
     <div class="inner">
-      <MovieItem 
-      v-for="movie in movies" 
-      :key="movie.imdbID"
-      :movie = "movie" />
+      <div class="message">
+        {{ message }}
+      </div>
+      <div class="movies">
+        <MovieItem 
+         v-for="movie in movies" 
+         :key="movie.imdbID"
+         :movie = "movie" /> <!--prop 명을 movie로, movie 객체를 넘겨주겠다. -->
+      </div>
     </div>
   </div>
 </template>
@@ -17,8 +22,21 @@ export default {
   },
   computed : {
     movies() {
-      return this.$store.state.movie.movies
+      return this.$store.state.movie.movies //여기서 this는 정확히 어딜 가리키나..? src폴더? 그리고 왜 모듈명이 아닌 state를 먼저 적나?
+    },
+    message() {
+      return this.$store.state.movie.message
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.container{
+  .movies{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+}
+</style>
