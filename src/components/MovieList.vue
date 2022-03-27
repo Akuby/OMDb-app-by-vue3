0ad65fa4a183eff1248311d11944dd477d863c8d
Akuby <akuby1997@gmail.com>
2022-03-27 20:37:06 +0900

    adding spinner

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,53 +1,67 @@
 <template>
   <div class="container">
-    <div class="inner">
+    <div :class="{'no-result' : !movies.length}" class="inner">
       <div 
-      v-if="message"
-      class="message">
+      v-if="loading"
+      class="spinner-border text-primary"></div>
+      <div v-if="message" class="message">
         {{ message }}
       </div>
-      <div 
-      v-else
-      class="movies">
-        <MovieItem 
-         v-for="movie in movies" 
-         :key="movie.imdbID"
-         :movie = "movie" /> <!--prop 명을 movie로, movie 객체를 넘겨주겠다. -->
+      <div v-else class="movies">
+        <MovieItem v-for="movie in movies" :key="movie.imdbID" :movie="movie" />
+        <!--prop 명을 movie로, "movie" 객체를 넘겨주겠다. -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import MovieItem from '~/components/MovieItem'
-export default {
-  components : {
-    MovieItem
-  },
-  computed : {
-    movies() {
-      return this.$store.state.movie.movies //여기서 this는 정확히 어딜 가리키나..? src폴더? 그리고 왜 모듈명이 아닌 state를 먼저 적나?
+  import MovieItem from '~/components/MovieItem'
+  export default {
+    components: {
+      MovieItem
     },
-    message() {
-      return this.$store.state.movie.message
+    computed: {
+      movies() {
+        return this.$store.state.movie.movies //여기서 this는 정확히 어딜 가리키나..? src폴더? 그리고 왜 모듈명이 아닌 state를 먼저 적나?
+      },
+      message() {
+        return this.$store.state.movie.message
+      },
+      loading() {
+        return this.$store.state.movie.loading
+      }
     }
   }
-}
 </script>
 
 <style lang="scss" scoped>
-@import "~/scss/main";
-.container{
-  margin-top: 30px;
-  .inner{
-    background-color: $gray-200;
-    padding: 10px 0;
-    border-radius: 4px;
-    .movies{
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
+  @import "~/scss/main";
+
+  .container {
+    margin-top: 30px;
+    padding-bottom: 30px;
+
+    .inner {
+      background-color: $gray-200;
+      padding: 10px 0;
+      border-radius: 4px;
+      text-align: center;
+
+      &.no-result {
+        padding: 70px 0;
+      }
+      .message {
+        color: $gray-400;
+        font-size: 20px;
+        font-family: "Roboto Slab", sans-serif;
+      }
+
+      .movies {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+      }
+    }
   }
-  }
-}
 </style>
