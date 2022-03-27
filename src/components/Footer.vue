9e91ea66d813487626630a1d356371739356d31b
Akuby <akuby1997@gmail.com>
2022-03-27 20:36:26 +0900

    new Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer>
+    <Logo></Logo>
+    <a href="https://github.com/Akuby"
+    target="_blank">
+    (c){{ new Date().getFullYear() }} Akuby
+    </a>
+  </footer>
+</template>
+
+<script>
+import Logo from '~/components/Logo'
+export default {
+  components:{
+    Logo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+footer{
+  padding: 70px 0;
+  text-align: center;
+  opacity: .3;
+  .logo{
+    display: block;//Logo 컴포넌트의 RouterLink는 a태그로 변환된다.
+    margin-bottom: 4px;
+  }
+}
+</style>
