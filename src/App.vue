55d3220c7b2faad333eb97ab6939f7c11c7b7376
Akuby <akuby1997@gmail.com>
2022-03-29 00:01:39 +0900

    footer injected

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <Header /> <!-- 헤더 -->
   <RouterView /> <!-- 헤더에 따른 페이지 컨텐츠 영역 -->
+  <Footer /> <!-- 푸터 -->
 </template>
 
 <script>
 import Header from '~/components/Header'
+import Footer from '~/components/Footer'
 export default {
   components : {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
